8d0659f37e5474f29724be4201044317f7903824
Oliver Reed <legowars42@yahoo.com>
2013-04-15 21:04:28 -0600

    added postinit

--- src/mods/harvestcraft/HarvestCraft.java
+++ src/mods/harvestcraft/HarvestCraft.java
@@ -2,11 +2,13 @@ package mods.harvestcraft;
 
 import mods.harvestcraft.proxy.CommonProxy;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkRegistry;
@@ -14,7 +16,8 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid="HarvestCraft", version="Alpha 0.0.1", useMetadata=true)
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
-public class HarvestCraft {
+public class HarvestCraft 
+{
 	
 	@Instance("HarvestCraft")
 	public static HarvestCraft instance;
@@ -24,14 +27,16 @@ public class HarvestCraft {
 	public static CommonProxy proxy;
 	
 	@PreInit
-	public void preInit(FMLPreInitializationEvent event) {
+	public void preInit(FMLPreInitializationEvent event) 
+	{
 		
 		Config.load(event.getSuggestedConfigurationFile());
 		
 	}
 	
 	@Init
-	public void load(FMLInitializationEvent event) {
+	public void load(FMLInitializationEvent event) 
+	{
 		
 		// Most of the mod initialization goes here, like
 		// instantiating the blocks and items, registering
@@ -40,6 +45,12 @@ public class HarvestCraft {
 		// Sciguymjm thinks it's ok too
 		
 		proxy.init();
+	
+	}
+		
+	@PostInit
+	public void postInit(FMLPostInitializationEvent event)
+	{
 		
 	}
 	
