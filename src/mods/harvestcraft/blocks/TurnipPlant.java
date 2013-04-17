b46ffe6d46e92f58ab6cd36b0c283d956264347c
David Carney <david.ca1226@yahoo.com>
2013-04-17 10:41:40 -0600

    Added growing...(?)

--- src/mods/harvestcraft/blocks/TurnipPlant.java
+++ src/mods/harvestcraft/blocks/TurnipPlant.java
@@ -1,5 +1,7 @@
 package mods.harvestcraft.blocks;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.material.Material;
@@ -48,5 +50,17 @@ public class TurnipPlant extends Block
 	public boolean isOpaqueCube()
 	{
 		return false;
+	}
+	@Override
+	public void updateTick (World world, int x, int y, int z, Random random) {
+		if (world.getBlockMetadata(x, y, z) == 1) {
+			return;
+		}
+
+		if (random.nextInt(isFertile(world, x, y - 1, z) ? 12 : 25) != 0) {
+			return;
+		}
+
+		world.setBlockMetadataWithNotify(x, y, z, 1);
 	}
 }
